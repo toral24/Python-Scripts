ab4757f0da941a5ae24d5261844899675b4f0899
Sergio Toral García <103923255+toral24@users.noreply.github.com>
2024-01-02 20:28:00 +0100

    2

--- FastAPI/users.py
+++ FastAPI/users.py
@@ -8,19 +8,37 @@ app = FastAPI()
 #Entidad user
 
 class User(BaseModel):
+    id: int
     nombre: str
     apellido: str
     edad: int
 
-users_list = [User(nombre="Sergio", apellido="Toral", edad="25"),
-         User(nombre="Sandra", apellido="Toral", edad="28"),
-         User(nombre="Carmen", apellido="Nicolas", edad="24")]
+users_list = [User(id=1, nombre="Sergio", apellido="Toral", edad="25"),
+            User(id=2, nombre="Sandra", apellido="Toral", edad="28"),
+            User(id=3, nombre="Carmen", apellido="Nicolas", edad="24")]
 
 @app.get("/usersjson")
 async def usersjson():
     return [{"nombre": "Sergio"},
             {"nombre": "Pedro"},
             {"nombre": "Pablo"}]
-@app.get("/users")
+
+@app.get("/users/")
 async def users():
     return users_list
+
+# Función para buscar usuarios en la lista
+def search_user(id: int):
+    Users = filter(lambda user: user.id == id, users_list)
+    try:
+        return list(Users)[0]
+    except:
+        return {"error": "No se ha encontrado el usuario"} 
+#path
+@app.get("/user/{id}")
+async def user(id: int):
+    return search_user(id)
+#query
+@app.get("/userquery/")
+async def user(id: int):
+    return search_user(id)
